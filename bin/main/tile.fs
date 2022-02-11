5657a7e3acd477fe29ad9aae4cf2e3706fb86962
green-new <tristenwoodruff2@gmail.com>
2022-02-11 01:13:48 -0500

    Fixing some stuff ig

---
+++ bin/main/tile.fs
@@ -0,0 +1,18 @@
+#version 300 es
+
+out vec4 FragColor;
+
+in vec3 outColor;
+
+uniform vec2 u_resolution;
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    float pct = 0.0;
+
+    pct = distance(st, vec2(0.0,0.0));
+
+    vec3 color = vec3(pct);
+
+    FragColor = vec4(color * outColor, 1.0);
+}
